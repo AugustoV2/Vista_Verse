6671dd7f529823df3c9d74aeff5e582c658bf37c
AugustoV2 <augustovarghese@gmail.com>
2025-02-24 01:15:29 +0530

    ee

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -34,7 +34,7 @@ const Navbar = () => {
                   <div className="w-8 h-8 bg-blue-100 rounded-full flex items-center justify-center">
                     <User className="h-5 w-5 text-blue-600" />
                   </div>
-                  <span>John Doe</span>
+                  <span>Amarnath C</span>
                 </button>
               </div>
 
@@ -68,7 +68,7 @@ const Navbar = () => {
                 <div className="w-8 h-8 bg-blue-100 rounded-full flex items-center justify-center">
                   <User className="h-5 w-5 text-blue-600" />
                 </div>
-                <span className="text-gray-700">John Doe</span>
+                <span className="text-gray-700">Amarnath C</span>
               </div>
               <a href="#" className="block px-3 py-2 text-gray-700 hover:text-blue-600">
                 Community Forum
